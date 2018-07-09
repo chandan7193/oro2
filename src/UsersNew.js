5db382ac1c9faaa489ece54a9bff51305ab5c0be
Chandan Singh <chandan.singh7193@gmail.com>
2018-07-09 19:19:26 +0530

    Half color done on UserNEw screen

--- src/UsersNew.js
+++ src/UsersNew.js
@@ -1,108 +1,138 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { Link } from "react-router-dom";
-import Header from './Header'
+import Header from "./Header";
 
-class UsersNew extends Component{
-	handleSubmit = e =>{
-		e.preventDefault();
-		const name = this.getName.value;
-    	const email = this.getEmail.value;
-    	const facebook =  this.getFacebook.value;
-    	const twitter = this.getTwitter.value;
-    	const linkedin = this.getLinkedIn.value;
+class UsersNew extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const name = this.getName.value;
+    const email = this.getEmail.value;
+    const facebook = this.getFacebook.value;
+    const twitter = this.getTwitter.value;
+    const linkedin = this.getLinkedIn.value;
 
-	    const data = {
-	      id: new Date(),
-	      name,
-	      email,
-	      facebook,
-	      twitter,
-	      linkedin,
-	      editing: false
-	    };
-	    this.props.dispatch({
-      		type: "ADD_USER",
-      		data
-	    });
-	    
-	    this.getName.value = "";
-	    this.getEmail.value = "";
-	    this.getFacebook.value = "";
-	    this.getTwitter.value = "";
-	    this.getLinkedIn.value = "";
-	    console.log('data',data);
-	}
+    const data = {
+      id: new Date(),
+      name,
+      email,
+      facebook,
+      twitter,
+      linkedin,
+      editing: false
+    };
+    this.props.dispatch({
+      type: "ADD_USER",
+      data
+    });
 
-	render(){
-		console.log("success")
-		return(
-			<div>
-			<Header/>
-			<div className="container">
-			<h1>Create Post</h1>
-	        <form onSubmit={this.handleSubmit}>
-	        <div className="row">
-              <div className="col-md-3">
-	          <input
-	            required
-	            type="text"
-	            ref={input => (this.getName = input)}
-	            placeholder="Name"
-	          />
-	          </div>
+    this.getName.value = "";
+    this.getEmail.value = "";
+    this.getFacebook.value = "";
+    this.getTwitter.value = "";
+    this.getLinkedIn.value = "";
+    console.log("data", data);
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="row">
+          <div className="col-md-2 col-6">
+            <div className="halfrounded">
+              <p style={{ marginTop: "1rem", backgroundColor: "#D6E9FF" }}>
+                Personal Info
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="col-md-4 col-12">
+                <label className="labelling" className="labelling">
+                  Name
+                </label>
+                <br />
+                <input
+                  required
+                  type="text"
+                  ref={input => (this.getName = input)}
+                  className="form-control"
+                />
+              </div>
             </div>
             <div className="row">
-              <div className="col-md-3">
-	          <input
-	            required
-	            type="text"
-	            ref={input => (this.getEmail = input)}
-	            placeholder="Email"
-	          />
-	          </div>
+              <div className="col-md-4 col-12">
+                <label className="labelling">Email</label>
+                <br />
+                <input
+                  required
+                  type="text"
+                  ref={input => (this.getEmail = input)}
+                  className="form-control"
+                />
+              </div>
             </div>
             <div className="row">
-              <div className="col-md-3">
-	          <input
-	            required
-	            type="text"
-	            ref={input => (this.getFacebook = input)}
-	            placeholder="Facebook"
-	          />
-	          </div>
+              <div className="col-md-4 col-12">
+                <label className="labelling">Facebook</label>
+                <br />
+                <input
+                  required
+                  type="text"
+                  ref={input => (this.getFacebook = input)}
+                  className="form-control"
+                />
+              </div>
+
+              <div className="col-md-4 col-12">
+                <label className="labelling">Twitter</label>
+                <br />
+                <input
+                  required
+                  type="text"
+                  ref={input => (this.getTwitter = input)}
+                  className="form-control"
+                />
+              </div>
+
+              <div className="col-md-4 col-12">
+                <label className="labelling">LinkedIn</label>
+                <br />
+                <input
+                  required
+                  type="text"
+                  ref={input => (this.getLinkedIn = input)}
+                  className="form-control"
+                />
+              </div>
             </div>
-	          
-	         <div className="row">
-              <div className="col-md-3">
-	          <input
-	            required
-	            type="text"
-	            ref={input => (this.getTwitter = input)}
-	            placeholder="Twitter"
-	          />
-	          </div>
+
+            <br />
+            <br />
+            <div className="float-right">
+              <button
+                className="btn btn-primary"
+                type="submit"
+                style={{ width: "150px", backgroundColor: "#4397FF" }}>
+                Save
+              </button>
+              &nbsp;&nbsp;&nbsp;&nbsp;
+              <Link
+                to="/"
+                className="btn btn-danger"
+                style={{ width: "150px", backgroundColor: "#4397FF" }}>
+                Next
+              </Link>
             </div>
-            <div className="row">
-              <div className="col-md-3">
-	          <input
-	            required
-	            type="text"
-	            ref={input => (this.getLinkedIn = input)}
-	            placeholder="LinkedIn"
-	          />
-	          </div>
-            </div>
-	          <br />
-	          <br />
-	          <button>Post</button>
-	        </form>
-	        </div>
+          </form>
+        </div>
       </div>
-
-		)
-	}
+    );
+  }
 }
 
 export default connect()(UsersNew);
